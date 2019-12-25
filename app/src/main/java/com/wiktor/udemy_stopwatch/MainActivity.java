0f80822336e9782d1059c31025f5f60a8bfed8c4
2wiktor2 <wiktor2011@gmail.com>
2019-12-25 10:10:31 +0300

    first

--- app/src/main/java/com/wiktor/udemy_stopwatch/MainActivity.java
+++ app/src/main/java/com/wiktor/udemy_stopwatch/MainActivity.java
@@ -3,12 +3,62 @@ package com.wiktor.udemy_stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textViewTime;
+    Button buttonStart;
+    Button buttonPause;
+    Button buttonReset;
+
+    private int seconds = 0;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewTime = findViewById(R.id.tv_time);
+        buttonStart = findViewById(R.id.b_start);
+        buttonPause = findViewById(R.id.b_pause);
+        buttonReset = findViewById(R.id.b_reset);
+
+        buttonStart.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+        buttonReset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.b_start:
+                isRunning = true;
+                break;
+            case R.id.b_pause:
+                isRunning = false;
+                break;
+            case R.id.b_reset:
+                isRunning = false;
+                seconds = 0;
+                break;
+        }
+    }
+
+    private void runTimer() {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        String time = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, secs);
+
+        if (isRunning) {
+            seconds++;
+        }
     }
 }
