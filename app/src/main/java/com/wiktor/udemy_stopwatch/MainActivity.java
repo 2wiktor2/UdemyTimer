54375bda0be540c1109e921e199da72a23983aae
2wiktor2 <wiktor2011@gmail.com>
2019-12-29 14:39:03 +0300

    lesson 43 finished.

--- app/src/main/java/com/wiktor/udemy_stopwatch/MainActivity.java
+++ app/src/main/java/com/wiktor/udemy_stopwatch/MainActivity.java
@@ -49,15 +49,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStop() {
         super.onStop();
-        wasRunning = isRunning;
-        isRunning =false;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         isRunning = wasRunning;
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = isRunning;
+        isRunning = false;
+    }
+
 
     @Override
     protected void onRestart() {
